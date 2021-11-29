126ecb05ea93e75e41d5edecb2467d9bd3acc349
ferdyhape <ferdyhahan5@gmail.com>
2021-11-29 16:21:49 +0700

    merelasikan tabel barang dan category

---
+++ app/Models/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+    ];
+
+    protected $guarded = ['id'];
+
+    public function barang()
+    {
+        return $this->hasMany(Barang::class);
+    }
+}
